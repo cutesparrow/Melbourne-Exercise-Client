4212c107944b71252e36628f7f7e206983f99c5d
Ziven <shigaoyu615@gmail.com>
2021-03-09 18:14:02 +1100

    Initial Commit

---
+++ MelbourneRun/MelbourneRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelbourneRunApp.swift
+//  MelbourneRun
+//
+//  Created by gaoyu shi on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MelbourneRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
