22127fdfd963fa249c730ee4183a53b51dd73c0a
Ziven <shigaoyu615@gmail.com>
2021-05-24 17:04:41 +1000

    add images

---
+++ MelbourneRun/Views/Gym/AnnotationView.swift
@@ -0,0 +1,20 @@
+//
+//  AnnotationView.swift
+//  MelbExercise
+//
+//  Created by gaoyu shi on 19/5/21.
+//
+
+import SwiftUI
+
+struct AnnotationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnnotationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnnotationView()
+    }
+}
---
+++ MelbourneRun/Views/Gym/GymImage.swift
@@ -0,0 +1,20 @@
+//
+//  GymImage.swift
+//  MelbExercise
+//
+//  Created by gaoyu shi on 19/5/21.
+//
+
+import SwiftUI
+
+struct GymImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GymImage_Previews: PreviewProvider {
+    static var previews: some View {
+        GymImage()
+    }
+}
